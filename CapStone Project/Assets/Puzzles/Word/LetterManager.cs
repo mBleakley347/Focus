731337de694ed021fb5c09287fc4af0975d96227
Michael Bleakley <1013532@student.sae.edu.au>
2019-06-13 15:12:50 +1000

    MB Started Letters Puzzle

---
+++ CapStone Project/Assets/Puzzles/Word/LetterManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterManager : MonoBehaviour
+{
+    
+    public LetterClass[] letters;
+    public LetterSpace[] space;
+    public int pos;
+    
+    public bool CheckLetterSpace(int posTemp)
+    {
+        if (space[posTemp] == null)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ CapStone Project/Assets/Puzzles/Word/LetterSpace.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterSpace : MonoBehaviour
+{
+
+    public LetterClass letter;
+    [SerializeField] private LetterManager wordPlacement;
+    public int pos;
+
+    public void MoveLetterUp()
+    {
+        int posTemp = pos - wordPlacement.pos;
+        if (CheckLetterSpace(posTemp))
+        {
+            wordPlacement.letters[posTemp] = letter;
+            letter.gameObject.transform.Translate(0, wordPlacement.gameObject.transform.position.y,0);
+            letter = null;
+        }
+    }
+
+    public bool CheckLetterSpace(int posTemp)
+    {
+        if (wordPlacement.letters[posTemp] == null)
+        {
+            return true;
+        }
+        return false;
+    }
+}
