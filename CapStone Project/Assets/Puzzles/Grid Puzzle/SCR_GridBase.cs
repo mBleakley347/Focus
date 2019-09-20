57a979e23c91d50f849ba9c40ded2eee52f5556a
Michael <1013532@student.sae.edu.au>
2019-09-20 14:11:54 +0800

    MB Grid puzzle

---
+++ CapStone Project/Assets/Puzzles/Grid Puzzle/SCR_GridBase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_GridBase : SCR_PuzzleComponent
+{
+    public int maxX;
+    public int maxY;
+    [SerializeField] private int currentX;
+    [SerializeField] private int currentY;
+    [SerializeField] private int correctX;
+    [SerializeField] private int correctY;
+    [SerializeField] private GameObject node;
+    private Vector3 newPos;
+
+    private void Awake()
+    {
+        newPos = node.transform.localPosition;
+    }
+
+    // Start is called before the first frame update
+    public void MoveLeft()
+    {
+        if (currentX == maxX)
+        {
+            return;
+        }
+        currentX += 1;
+        newPos.x += 1;
+        Check();
+    }
+    public void MoveRight()
+    {
+        if (currentX == 0)
+        {
+            return;
+        }
+        currentX -= 1;
+        newPos.x -= 1;
+        Check();
+    }
+    public void MoveUp()
+    {
+        if (currentY == maxY)
+        {
+            return;
+        }
+        currentY += 1;
+        newPos.y += 1;
+        Check();
+    }
+    public void MoveDown()
+    {
+        if (currentY == 0)
+        {
+            return;
+        }
+        currentY -= 1;
+        newPos.y -= 1;
+        Check();
+    }
+
+    private void Update()
+    {
+        node.transform.localPosition =
+            Vector3.Lerp(node.transform.localPosition, newPos, Time.deltaTime);
+    }
+
+    private void Check()
+    {
+        if (currentX == correctX && currentY == correctY)
+        {
+            complete = true;
+        }
+    }
+}
---
+++ CapStone Project/Assets/Puzzles/Grid Puzzle/SCR_Wheel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
+
+public class SCR_Wheel : SCR_PuzzleComponent
+{
+    [SerializeField] private SCR_GridBase grid;
+    [SerializeField] private Vector3 pos;
+    [SerializeField] private SCR_PlayerController player;
+    [SerializeField] private bool directionY;
+    [SerializeField] private float resetTime;
+    [SerializeField] private float distance;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private Vector3 newPos;
+    [SerializeField] private GameObject graphic;
+    private Vector3 nodePosition;
+    
+    public Vector3 newRotation;
+    public Quaternion newQuaternion;
+
+    private void Awake()
+    {
+        newRotation = graphic.transform.eulerAngles;
+    }
+
+    public override bool Click(Vector3 a)
+    {
+        pos = a;
+        return base.Click(a);
+    }
+
+    public override bool Hold(Vector3 a)
+    {
+        a -= pos;
+        Vector3 FacePos = Vector3.ProjectOnPlane(transform.InverseTransformPoint(a), transform.forward);
+        FacePos.Normalize();
+        if (a.x < FacePos.x - 1 && !directionY || directionY && a.y > FacePos.y + 2)
+        {
+            MoveNode(true);
+            player.ForceMouseUp(resetTime);
+            newRotation = new Vector3(graphic.transform.eulerAngles.x,graphic.transform.eulerAngles.y, newRotation.z +90);
+            newQuaternion = Quaternion.Euler(newRotation);
+            newRotation = newQuaternion.eulerAngles;
+        } else if (a.x > FacePos.x + 2 && !directionY || directionY && a.y < FacePos.y - 1)
+        {
+            MoveNode(false);
+            player.ForceMouseUp(resetTime);
+            newRotation = new Vector3(graphic.transform.eulerAngles.x,graphic.transform.eulerAngles.y, newRotation.z -90);
+            newQuaternion = Quaternion.Euler(newRotation);
+            newRotation = newQuaternion.eulerAngles;
+        }
+        
+
+        return base.Hold(a);
+    }
+
+    private void MoveNode(bool positive)
+    {
+        if (!directionY)
+        {
+            if (positive)
+            {
+                grid.MoveRight();
+            }
+            else
+            {
+                grid.MoveLeft();
+            }
+        }
+        else
+        {
+            if (positive)
+            {
+                grid.MoveUp();
+            }
+            else
+            {
+                grid.MoveDown();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (graphic.transform.rotation.z < newQuaternion.z || graphic.transform.rotation.z > newQuaternion.z)
+        {
+            graphic.transform.rotation = Quaternion.Lerp(graphic.transform.rotation, newQuaternion, rotationSpeed);
+        }
+    }
+}
