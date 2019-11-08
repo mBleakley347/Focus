0e023b128e63f3c6a930430f18198801d420ecb1
Michael Bleakley <1013532@student.sae.edu.au>
2019-11-08 14:48:42 +1100

    MB Static turn script

---
+++ CapStone Project/Assets/Scripts/SCR_StaticTurn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_StaticTurn : MonoBehaviour
+{
+    [SerializeField]private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player.transform);
+        float temp = Mathf.Repeat(transform.localEulerAngles.y + 180,360);
+        transform.localRotation = Quaternion.Euler(0, Mathf.Clamp(temp, 140, 220), 0);
+        
+    }
+}
