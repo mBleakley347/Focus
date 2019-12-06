8c1ada311801f4239e9de0ddfa592a1d0f5cf240
Uncanny <notebucket@yahoo.com>
2019-12-06 12:02:58 +1100

    remark and transition to memory scripts

--- CapStone Project/Assets/Scripts/Managers/GameManager/GeneralScripts/Memory.cs
+++ CapStone Project/Assets/Scripts/Managers/GameManager/GeneralScripts/Memory.cs
@@ -6,10 +6,56 @@ public class Memory : SCR_Interactable
 {
     public GameObject memoryManager;
     public GameObject dadsgroup;
+    
+    public AudioClip remark;
+    public float waittime = 1;
+    public float whiteouttime = 0.2f;
+    public List<GameObject> whiteouts;
+    private bool start = true;
+    private bool reverse = false;
+    private int working = 0;
     public override bool Click(Vector3 a)
     {
-        memoryManager.SetActive(true);
-        dadsgroup.SetActive(true);
+        StartCoroutine(WaitForAudio());
         return base.Click(a);
+    }
+
+    IEnumerator WaitForAudio()
+    {
+        if (start)
+        {
+            if(remark)
+                SCR_AudioManager.instanceAM.voiceSouce.PlayOneShot(remark);
+            start = false;
+            yield return new WaitForSeconds(waittime);
+        }
+        // handle timed transition to black here
+        if (working < whiteouts.Count)
+        {
+            if(whiteouts.Count!=0)
+                whiteouts[working]?.SetActive(true);
+            working++;
+        }
+        else
+        {
+            if (working >= whiteouts.Count)
+            {
+                reverse = true;
+                memoryManager.SetActive(true);
+                dadsgroup.SetActive(true);
+            }
+
+            if (reverse&&working>-1)
+            {
+                if(whiteouts.Count!=0)
+                    whiteouts[working]?.SetActive(false);
+                working--;
+            }
+            else if(working==-1)
+            {
+                StopCoroutine("WaitForAudio");
+            }
+        }
+        yield return new WaitForSeconds(whiteouttime);
     }
 }
